ff5b0b866f6b12b34da49e6d00bc5d31fae9239f
payssion <github@payssion.com>
2017-07-06 20:25:59 +0800

    update pm_id for sample

--- samples/sample_create.php
+++ samples/sample_create.php
@@ -10,7 +10,7 @@ try {
 	$response = $payssion->create(array(
 			'amount' => 1,
 			'currency' => 'USD',
-			'pm_id' => 'hsbc_br',
+			'pm_id' => 'alipay_cn',
 			'description' => 'order description',
 			'order_id' => 'your order id',          //your order id
 			'return_url' => 'your return url'   //optional, the return url after payments (for both of paid and non-paid)
